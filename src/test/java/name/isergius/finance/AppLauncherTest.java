ed393d22c4704641cda4adbbb5bac9d1361579d6
Sergey Kondratyev <ron.rcf@gmail.com>
2016-10-30 00:09:51 +0500

    Created test to verify the availability of the application.

---
+++ src/test/java/name/isergius/finance/AppLauncherTest.java
@@ -0,0 +1,28 @@
+package name.isergius.finance;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by isergius on 28.10.16.
+ */
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class AppLauncherTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testAppLauncher() {
+        String expected = "pong";
+        String actual = this.restTemplate.getForObject("/ping", String.class);
+        Assert.assertEquals(expected, actual);
+    }
+}
